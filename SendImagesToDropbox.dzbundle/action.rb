74f35d2c5fe7c38a444be7e799675212d5b35c29
Yuki Yamashina <loveoasisforever@gmail.com>
2014-12-04 10:29:55 +0900

    convert tabs to spaces

--- SendImagesToDropbox.dzbundle/action.rb
+++ SendImagesToDropbox.dzbundle/action.rb
@@ -23,73 +23,73 @@ require 'fileutils'
 @dropboxPublicBaseURL = "http://dl.dropbox.com/u/"
 
 def dropbox?
-	# Get Dropbox Public directory from the Dropbox database file, if this file is not present
-	# Dropbox is most likely not installed
-	if File.exist?(conf_path = ENV['HOME'] + '/.dropbox/host.db')
-	  @dropboxPubDir = Base64.decode64(File.open(conf_path).read).match(/\/.+$/)[0] + '/Public'
-	  return true
+  # Get Dropbox Public directory from the Dropbox database file, if this file is not present
+  # Dropbox is most likely not installed
+  if File.exist?(conf_path = ENV['HOME'] + '/.dropbox/host.db')
+    @dropboxPubDir = Base64.decode64(File.open(conf_path).read).match(/\/.+$/)[0] + '/Public'
+    return true
   else
     return false
   end
 end
 
 def dragged
-	$dz.determinate(true)
-	# Check if Dropbox is installed and set Public path
-	if not dropbox?
-		$dz.finish("Dropbox is not installed")
-		$dz.url(false)
-		return
-	end
+  $dz.determinate(true)
+  # Check if Dropbox is installed and set Public path
+  if not dropbox?
+    $dz.finish("Dropbox is not installed")
+    $dz.url(false)
+    return
+  end
 
-	# Handle Drag
-	path = []
-	if File.directory?($items[0])
-		# 1 Folder was dragged
-		Dir::entries($items[0]).each do |item|
-			if item !~ /^\..*/
-				file_path = Pathname($items[0]) + item
-				path << generate_random_name_file(file_path.to_s)
-			end
+  # Handle Drag
+  path = []
+  if File.directory?($items[0])
+    # 1 Folder was dragged
+    Dir::entries($items[0]).each do |item|
+      if item !~ /^\..*/
+        file_path = Pathname($items[0]) + item
+        path << generate_random_name_file(file_path.to_s)
+      end
 
-		end
-	else 
-		# 1 item or More than 1 item dragged 
-		$items.each do |item| 
-			path << generate_random_name_file(item)
-		end
-	end
+    end
+  else 
+    # 1 item or More than 1 item dragged 
+    $items.each do |item| 
+      path << generate_random_name_file(item)
+    end
+  end
 
-	# Copy file to Dropbox Public dir and place create URL on Clipboard
-	$dz.begin("Copying files ...")
-	path.each do |file_path|
-		Rsync.do_copy([file_path], @dropboxPubDir, false)
-		$dz.url("#{@dropboxPublicBaseURL}#{ENV['user_id']}/#{File.basename(file_path)}")
-		FileUtils.rm_f(file_path)
-	end
-	$dz.finish("URLs is now on clipboard")
+  # Copy file to Dropbox Public dir and place create URL on Clipboard
+  $dz.begin("Copying files ...")
+  path.each do |file_path|
+    Rsync.do_copy([file_path], @dropboxPubDir, false)
+    $dz.url("#{@dropboxPublicBaseURL}#{ENV['user_id']}/#{File.basename(file_path)}")
+    FileUtils.rm_f(file_path)
+  end
+  $dz.finish("URLs is now on clipboard")
 end
 
 def clicked
-	# Check for Dropbox and set Public Directory path
-	if not dropbox?
-		$dz.determinate(false)
-		$dz.finish("Dropbox is not installed")
-		$dz.url(false)
-	else
-		# Open Finder at Public Directory using Applescript
-		`osascript -e 'tell application "Finder"' -e 'activate' -e 'open folder\
-		POSIX file "#{@dropboxPubDir}"' -e 'end tell'`
-	end
+  # Check for Dropbox and set Public Directory path
+  if not dropbox?
+    $dz.determinate(false)
+    $dz.finish("Dropbox is not installed")
+    $dz.url(false)
+  else
+    # Open Finder at Public Directory using Applescript
+    `osascript -e 'tell application "Finder"' -e 'activate' -e 'open folder\
+    POSIX file "#{@dropboxPubDir}"' -e 'end tell'`
+  end
 end
 
 def generate_random_name_file file
-	file_path = Pathname(file)
-	file_extension = file_path.basename.to_s.split('.').last
-	random_strings = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
-	random_file_name = (0..64).map { random_strings[rand(random_strings.length)] }.join + '.' + file_extension
-	random_file_path = Pathname('/tmp/') + random_file_name
-	FileUtils.cp(file, random_file_path)
+  file_path = Pathname(file)
+  file_extension = file_path.basename.to_s.split('.').last
+  random_strings = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
+  random_file_name = (0..64).map { random_strings[rand(random_strings.length)] }.join + '.' + file_extension
+  random_file_path = Pathname('/tmp/') + random_file_name
+  FileUtils.cp(file, random_file_path)
 
-	random_file_path.to_s
+  random_file_path.to_s
 end
